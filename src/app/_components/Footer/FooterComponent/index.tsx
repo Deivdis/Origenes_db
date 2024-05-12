57bf26e4062ea4f2533672f7b8a74d57b4d52dda
Deivids <ortegadeivids4@gmail.com>
2024-05-11 21:43:05 -0500

    Se suve este cabio porsi las moscas totales esta los cambio del login y el color del button.

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Footer, Media } from '../../../../payload/payload-types'
+import { ThemeSelector } from '../../../_providers/Theme/ThemeSelector'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
+import { Button } from '../../Button'
+import { Gutter } from '../../Gutter'
+import { CMSLink } from '../../Link'
+
+import '../../../_css/global.css'
+
+import classes from './index.module.scss'
+
+const FooterComponent = ({ footer }: { footer: Footer }) => {
+  const pathname = usePathname()
+  const navItems = footer?.navItems || []
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+
+      <div className={classes.footer}>
+        <Gutter>
+          <nav>
+            <div className="text-2xl">
+              <p>Orígenes</p>
+            </div>
+          </nav>
+          <nav className={classes.nav}>
+            <ThemeSelector />
+            {navItems.map(({ link }, i) => {
+              return <CMSLink key={i} {...link} />
+            })}
+            <Link href="https://github.com/Deivdis" target="_blank" rel="noopener noreferrer">
+              Deivids
+            </Link>
+            <nav className="space-x-5">
+              <p>Antonio </p>
+              <p>Jose </p>
+              <p>Arnold </p>
+            </nav>
+          </nav>
+          <div className={classes.wrap}>
+            <p>{footer?.copyright}</p>
+
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                // @ts-ignore
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterComponent
--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -27,24 +27,24 @@ export async function Header() {
         className="origenes-navbar text-lex backdrop-filter backdrop-blur-lg lg:justify-center z-30
             sticky top-0 width-[100vw] w-full py-1 sm:px-12 px-5 flex justify-between items-center"
       >
-        <div className="nav-content ">
+        <div className="nav-content">
           <div className="nav-logo">
             Origenes <span>(Home)</span>
           </div>
           <ul className="nav-items">
             <li className="max-sm:hidden">
-              <button disabled className="disabled:text-gray-400">
+              <button disabled className="disabled:text-gray-500">
                 Overview
               </button>
             </li>
-            <li className="max-sm:hidden">
+            <li className="max-sm:hidden text-gray-500">
               <button>Tech Specs</button>
             </li>
-            <li className="max-sm:hidden">
+            <li className="max-sm:hidden text-gray-500">
               <button>Compare</button>
             </li>
             <li>
-              <button className="buy-btn px-3 py-0 rounded-xl">Buy</button>
+              <button className="text-black buy-btn px-3 py-0 rounded-xl">Buy</button>
             </li>
           </ul>
         </div>
