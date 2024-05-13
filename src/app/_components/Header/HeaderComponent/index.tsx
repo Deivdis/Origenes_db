859bb0c6e6ca67579081abd051343be4e46b3bd9
Deivids <ortegadeivids4@gmail.com>
2024-05-13 12:44:52 -0500

    Al gunos retoque en el OrderConfirmation, CategoryCard, Se arreglo el CustonHero, AccountFrom

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -10,6 +10,8 @@ import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 import { HeaderNav } from '../Nav'
 
+import '../../../_css/global.css'
+
 import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
@@ -17,13 +19,18 @@ const HeaderComponent = ({ header }: { header: Header }) => {
 
   return (
     <nav
-      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
-        .filter(Boolean)
-        .join(' ')}
+      className="text-lex backdrop-filter backdrop-blur-lg lg:justify-center z-30
+      sticky top-0 width-[100vw] w-full py-1 sm:px-12 px-5 flex justify-between items-center"
     >
-      <Gutter className={classes.wrap}>
-        <HeaderNav header={header} />
-      </Gutter>
+      <nav
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        <Gutter className={classes.wrap}>
+          <HeaderNav header={header} />
+        </Gutter>
+      </nav>
     </nav>
   )
 }
--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -23,32 +23,6 @@ export async function Header() {
   return (
     <>
       <HeaderComponent header={header} />
-      <nav
-        className="origenes-navbar text-lex backdrop-filter backdrop-blur-lg lg:justify-center z-30
-            sticky top-0 width-[100vw] w-full py-1 sm:px-12 px-5 flex justify-between items-center"
-      >
-        <div className="nav-content">
-          <div className="nav-logo">
-            Origenes <span>(Home)</span>
-          </div>
-          <ul className="nav-items">
-            <li className="max-sm:hidden">
-              <button disabled className="disabled:text-gray-500">
-                Overview
-              </button>
-            </li>
-            <li className="max-sm:hidden text-gray-500">
-              <button>Tech Specs</button>
-            </li>
-            <li className="max-sm:hidden text-gray-500">
-              <button>Compare</button>
-            </li>
-            <li>
-              <button className="text-black buy-btn px-3 py-0 rounded-xl">Buy</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
     </>
   )
 }
